44a4a58002e3cd5297a346ecd46ea834d1c6233d
yungshun317 <seraph317@gmail.com>
2021-06-02 14:01:40 +0800

    [FEAT] Add the `createArray` utility function

--- src/StarRating.js
+++ src/StarRating.js
@@ -2,6 +2,9 @@ import React from "react";
 
 import Star from "./Star";
 
+const createArray = length => [...Array(length)];
+
+// [4] `StarRating` does not store states for color ratings
 export default function StarRating({ totalStars = 5, selectedStars = 0 }) {
 	return (
 		<>
