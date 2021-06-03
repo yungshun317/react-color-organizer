b451648591fb4d09507ae897b6a6c3e618e18012
yungshun317 <seraph317@gmail.com>
2021-06-03 10:29:44 +0800

    [FEAT] Use controlled component instead of refs to implement the form component

--- src/AddColorForm.js
+++ src/AddColorForm.js
@@ -1,6 +1,8 @@
-import React, { useRef } from "react";
+import React, { useState } from "react";
 
-// [6] Use refs which store values for the lifetime of a component 
+/* [6] Use `txtTitle` and `hexColor` refs
+import { useRef } from "react";
+
 export default function AddColorForm({ onNewColor = f => f }) {
 	const txtTitle = useRef();
 	const hexColor = useRef();
@@ -18,6 +20,39 @@ export default function AddColorForm({ onNewColor = f => f }) {
 		<form onSubmit={submit}>
 		    <input ref="txtTitle" type="text" placeholder="color title..." required />
 		    <input ref={hexColor} type="color" required />
+		    <button>ADD</button>
+		</form>
+	);
+}
+*/
+
+// [6] Use controlled component
+export default function AddColorForm({ onNewColor = f => f }) {
+	const [title, setTitle] = useState("");
+	const [color, setColor] = useState("#000000");
+
+	const submit = e => {
+		e.preventDefault();
+		onNewColor(title, color);
+		setTitle("");
+		setColor("");
+	};
+
+	return (
+		<form onSubmit={submit}>
+		    <input
+		        value={title}
+		        onChange={event => setTitle(event.target.value)}
+		        type="text"
+		        placeholder="color title..."
+		        required
+		    />
+		    <input
+		        value={color}
+		        onChange={event => setColor(event.target.value)}
+		        type="color"
+		        required
+		    />
 		    <button>ADD</button>
 		</form>
 	);
